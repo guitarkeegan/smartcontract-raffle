1913b3b99175197eedc8a5fd07045c4ba0d21285
guitarkeegan <keegananglim@gmail.com>
2022-10-17 13:06:34 -0700

    working on keepers now automationCompatibleInterface

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,22 +2,50 @@
 pragma solidity ^0.8.8;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
 error Raffle__NotEnoughEthEntered();
+error Raffle__TransferFailed();
 
 ///@author Keegan Anglim
 ///@title Raffle
-contract Raffle is VRFConsumerBaseV2{
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface{
+
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    } // uint256, 0 = OPEN, 1 = CALCULATING
+
     /* State variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
+    /* Lottery Variables */
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
+    
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
     }
 
     /* Events */
     event RaffleEntered(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
     ///@dev Sign up for raffle by sending required funds
     function enterRaffle() public payable {
@@ -27,11 +55,38 @@ contract Raffle is VRFConsumerBaseV2{
     }
 
     // external is cheaper, this contract can't call it
-    function requestRandomWinner() external view {
-
+    function requestRandomWinner() external {
+            uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override{}
+    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override{
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success){
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /**
+     * @dev This is the function that the chainlink Keeper (automation) nodes 
+     * call. They look for the upkeeperNeeded to return true.
+     * The following should be true in order to return true:
+     * 1. Time interval should have passed.
+     * 2. Lottery should have at least one player and have some ETH.
+     * 3. Subscription is funded with LINK.
+     * 4. The Lottery should be in an 'open' state. 
+     */
+    function checkUpkeep(bytes calldata checkData) external returns (bool upkeepNeeded, bytes memory performData){}
 
     /* view/pure functions */
     function getEntranceFee() public view returns(uint256){
@@ -40,5 +95,9 @@ contract Raffle is VRFConsumerBaseV2{
 
     function getPlayer(uint256 index) public view returns(address){
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns(address){
+        return s_recentWinner;
     }
 }
